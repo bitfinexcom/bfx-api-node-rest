c05d7d0d85ae29e2da6338651fcd10f8a54ab043
Cris Mihalache <me@f3rno.com>
2020-03-12 02:44:43 +0700

    (feature) generate JSDoc html documentation

--- test/lib/rest1.js
+++ test/lib/rest1.js
@@ -3,12 +3,16 @@
 /* eslint camelcase: "off" */
 /* eslint-disable no-unused-expressions */
 
-const assert = require('assert')
-const _isArray = require('lodash/isArray')
-const { expect } = require('chai')
 const DNS = require('dns')
+const assert = require('assert')
+const { expect } = require('chai')
+const _has = require('lodash/has')
+const _map = require('lodash/map')
+const _keys = require('lodash/keys')
+const _every = require('lodash/every')
+const _values = require('lodash/values')
+const _isArray = require('lodash/isArray')
 const RESTv1 = require('../../lib/rest1')
-const _ = require('lodash')
 
 describe('REST v1', () => {
   let skipPublic = process.env.SKIP_PUBLIC_REST
@@ -42,7 +46,8 @@ describe('REST v1', () => {
       bfx_rest.ticker('BTCUSD', (error, data) => {
         assert(!error)
         expect(data).to.exist
-        expect(_.has(data, ['mid',
+        expect(_has(data, [ // eslint-disable-line
+          'mid',
           'bid',
           'ask',
           'last_price',
@@ -68,9 +73,9 @@ describe('REST v1', () => {
       bfx_rest.stats('BTCUSD', (error, data) => {
         assert(!error)
         expect(data).to.exist
-        expect(_.has(data[0], ['period', 'volume']))
-        expect(_.has(data[1], ['period', 'volume']))
-        expect(_.has(data[2], ['period', 'volume']))
+        expect(_has(data[0], ['period', 'volume'])) // eslint-disable-line
+        expect(_has(data[1], ['period', 'volume'])) // eslint-disable-line
+        expect(_has(data[2], ['period', 'volume'])) // eslint-disable-line
         done()
       })
     })
@@ -80,10 +85,10 @@ describe('REST v1', () => {
       bfx_rest.fundingbook('USD', (error, data) => {
         assert(!error)
         expect(data).to.exist
-        expect(_.has(data, ['bids', 'asks']))
-        expect(_.keys(data.bids[0])).is.eql(['rate', 'amount', 'period', 'timestamp', 'frr'])
-        expect(_.keys(data.asks[0])).is.eql(['rate', 'amount', 'period', 'timestamp', 'frr'])
-        expect(_.every([data.asks[0] + data.bids[0]]), !NaN).ok
+        expect(_has(data, ['bids', 'asks'])) // eslint-disable-line
+        expect(_keys(data.bids[0])).is.eql(['rate', 'amount', 'period', 'timestamp', 'frr'])
+        expect(_keys(data.asks[0])).is.eql(['rate', 'amount', 'period', 'timestamp', 'frr'])
+        expect(_every([data.asks[0] + data.bids[0]]), !NaN).ok
         done()
       })
     })
@@ -94,10 +99,10 @@ describe('REST v1', () => {
       bfx_rest.orderbook('BTCUSD', (error, data) => {
         assert(!error)
         expect(data).to.exist
-        expect(_.keys(data)).is.eql(['bids', 'asks'])
-        expect(_.keys(data.bids[0])).is.eql(['price', 'amount', 'timestamp'])
-        expect(_.keys(data.asks[0])).is.eql(['price', 'amount', 'timestamp'])
-        expect(_.every([data.asks[0] + data.bids[0]]), !NaN).ok
+        expect(_keys(data)).is.eql(['bids', 'asks'])
+        expect(_keys(data.bids[0])).is.eql(['price', 'amount', 'timestamp'])
+        expect(_keys(data.asks[0])).is.eql(['price', 'amount', 'timestamp'])
+        expect(_every([data.asks[0] + data.bids[0]]), !NaN).ok
         done()
       })
     })
@@ -108,9 +113,9 @@ describe('REST v1', () => {
         assert(!error)
         assert(_isArray(data))
         expect(data.length).to.eql(100)
-        expect(_.keys(data[0])).to.eql(['timestamp', 'tid', 'price', 'amount', 'exchange', 'type'])
+        expect(_keys(data[0])).to.eql(['timestamp', 'tid', 'price', 'amount', 'exchange', 'type'])
         expect(
-          _.map(_.values(data[0]), (v) => typeof (v))
+          _map(_values(data[0]), (v) => typeof (v))
         ).is.eql(['number', 'number', 'string', 'string', 'string', 'string'])
         done()
       })
@@ -123,9 +128,9 @@ describe('REST v1', () => {
         expect(data).to.exist
         assert(_isArray(data))
         expect(data.length).to.eql(50)
-        expect(_.keys(data[0])).to.eql(['rate', 'amount_lent', 'amount_used', 'timestamp'])
+        expect(_keys(data[0])).to.eql(['rate', 'amount_lent', 'amount_used', 'timestamp'])
         expect(
-          _.map(_.values(data[0]), (v) => typeof (v))
+          _map(_values(data[0]), (v) => typeof (v))
         ).is.eql(['string', 'string', 'string', 'number'])
         done()
       })
